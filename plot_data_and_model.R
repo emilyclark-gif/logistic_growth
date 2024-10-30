65f29daee1a011de3ff621e88c5888ea61534d0e
Emily Clark <emily.clark@keble.ox.ac.uk>
2024-10-30 10:42:57 +0000

    commit

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,14 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
+
+## working out the growth rate
+data_subset3 <- growth_data %>% filter(t>0, t<1500) %>% mutate(N_log = log(N))
+
+model3 <- lm(N_log ~ t, data_subset3)
+summary(model3)
+
+## gives us a result of t estimate = 9.988e-03 which is our growth rate
 
 logistic_fun <- function(t) {
   
@@ -10,13 +18,13 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- 879 #
   
-r <- ??? #
+r <- 0.009988 #
   
-K <- ??? #
+K <- 6e+10 #
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
